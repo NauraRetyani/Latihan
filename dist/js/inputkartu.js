54ca6f959725d3015a34e5991664bf3c723eef5e
naura <naura.retyani@gmail.com>
2022-08-03 16:55:03 +0700

    upload image

--- dist/js/inputkartu.js
+++ dist/js/inputkartu.js
@@ -2,11 +2,19 @@ function isiData(){
 
   var nama = document.getElementById("input-name").value;
   var desc = document.getElementById("input-desc").value;
-  var img = document.getElementById("input-img").value;
+//  var img = $("#input-img").val();
+//  console.log(img);
 
 
   document.getElementById("nama").innerHTML = nama;
   document.getElementById("desc").innerHTML = desc;
-  document.getElementById("img-data").setAttribute("src", img);
-  document.getElementById("img-data").src="newSource.png";
+//  document.getElementById("img-data").setAttribute("src", img);
+
+  var input = document.getElementById("input-img");
+  var fReader = new FileReader();
+  fReader.readAsDataURL(input.files[0]);
+  fReader.onloadend = function(event){
+      var img = document.getElementById("img-data");
+      img.src = event.target.result;
+  }
 }
